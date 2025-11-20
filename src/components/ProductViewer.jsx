eb1c3bf48f511555d1444099d398af0088195217
Anna Baharyan <anna.baharyan@gohacktech.com>
2025-11-20 11:00:40 +0400

    create macbook 3d models

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,16 @@
+const ProductVier = () => {
+  return (
+    <section id="product-viewer">
+      <h2>Take a closer look.</h2>
+      <div className="controls">
+        <p className="info">MacBookPro 16” in Space Black</p>
+        <div className="flex-center gap-5 mt-5">
+          <div className="bg-natural-300" />
+          <div className="bg-natural-500" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductVier;
